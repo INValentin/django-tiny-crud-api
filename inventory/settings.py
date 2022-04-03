3887e5da94b6335fea794f6bfe7240dd23c58eee
INValentin <57767759+INValentin@users.noreply.github.com>
2022-04-03 23:06:10 +0200

    added static files settings

--- inventory/settings.py
+++ inventory/settings.py
@@ -134,11 +134,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
